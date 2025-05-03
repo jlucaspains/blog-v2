75f11929ff9f86c811620c48e00ce87bb1d69dff
jlucaspains <jlucaspains@gmail.com>
2025-05-03 13:43:13 -0600

    Sharp web insights integration

---
+++ assets/js/sharp-web-analytics.js
@@ -0,0 +1,16 @@
+const url = window.location.href;
+const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const referrer = document.referrer;
+const pageViewData = {
+    url,
+    timeZone,
+    timestamp: new Date().toISOString(),
+    referrer
+};
+fetch('https://sharp-web-insights.azurewebsites.net/api/V04xAkcndX/pageview', {
+    method: 'POST',
+    body: JSON.stringify(pageViewData),
+    headers: { 'Content-Type': 'application/json' }
+}).catch(error => {
+    console.error('Error sending page view data:', error);
+});
