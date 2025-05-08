9fd762c4dbc2bc4c2da9e24c7d3be3386ae87909
jlucaspains <jlucaspains@gmail.com>
2025-05-07 20:56:48 -0600

    Adjusted analytics script

--- assets/js/sharp-web-analytics.js
+++ assets/js/sharp-web-analytics.js
@@ -1,16 +1,55 @@
 const url = window.location.href;
 const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 const referrer = document.referrer;
+const loadDateTime = new Date();
+let pageViewId = "";
+
+addEventListener("beforeunload", (event) => {
+    if (pageViewId === "") {
+        return;
+    }
+
+    const unloadDateTime = new Date();
+    const diffMilliseconds = Math.abs(unloadDateTime - loadDateTime);
+    const diffSeconds = Math.round(diffMilliseconds / 1000);
+    const pageViewDurationData = {
+        id: pageViewId,
+        durationSeconds: diffSeconds,
+        timestamp: unloadDateTime.toISOString()
+    };
+    fetch('https://sharp-web-insights.azurewebsites.net/api/VovhZggVR6/pageViewDuration', {
+        method: 'POST',
+        body: JSON.stringify(pageViewDurationData),
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then(response => {
+            return response.text();
+        })
+        .then(data => {
+            pageViewId = data; // Store the page view ID for later use
+        })
+        .catch(error => {
+            console.error('Error sending page view duration data:', error);
+        });
+})
+
 const pageViewData = {
     url,
     timeZone,
-    timestamp: new Date().toISOString(),
+    timestamp: loadDateTime.toISOString(),
     referrer
 };
 fetch('https://sharp-web-insights.azurewebsites.net/api/VovhZggVR6/pageview', {
     method: 'POST',
     body: JSON.stringify(pageViewData),
     headers: { 'Content-Type': 'application/json' }
-}).catch(error => {
-    console.error('Error sending page view data:', error);
-});
+})
+    .then(response => {
+        return response.text();
+    })
+    .then(data => {
+        pageViewId = data; // Store the page view ID for later use
+    })
+    .catch(error => {
+        console.error('Error sending page view data:', error);
+    });
